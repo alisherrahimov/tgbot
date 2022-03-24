fc18501fbb70a8fe53abb37ee3f685e5b815630d
alisherrahimov <ooops2211@gmai.com>
2022-03-24 23:52:11 +0500

    12

--- db/connect.js
+++ db/connect.js
@@ -5,8 +5,7 @@ export const connectDB = async () => {
         await mongoose.connect("mongodb+srv://alisher:alisher2211@cluster0.smo7f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
         console.log("MongoDB Connected...");
     } catch (err) {
-        console.error(err.message);
-        // Exit process with failure
+        console.error(err);
         process.exit(1);
     }
 }
