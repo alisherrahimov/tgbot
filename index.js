cb4fb394849e87281e26cf2f1af6d8ed620a1c19
alisherrahimov <ooops2211@gmai.com>
2022-03-25 18:32:30 +0500

    asdds

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const bot = require("./server");
 const { webhookCallback } = require("grammy");
 const { config } = require("dotenv");
 config()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const app = express()
 let token = "5259090349:AAHQJzbGx-_mE1dmHec1IbP8F97t2ByIdXk"
 app.use(cors())
 app.use(express.json())
 app.use(`/${token}`, webhookCallback(bot, "express"));
-app.listen(port, async () => {
+app.listen(port, '0.0.0.0', async () => {
     await bot.api.setWebhook(`https://tgbotenglish.herokuapp.com/${token}`);
 });
