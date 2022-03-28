4d18654abb249a30dd8ac38ed689e0b3a8bb31a5
alisherrahimov <ooops2211@gmai.com>
2022-03-28 13:30:34 +0500

    asa

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// const express = require("express");
-// const cors = require("cors");
-// const bot = require("./server");
-// const { webhookCallback } = require("grammy");
-// const { config } = require("dotenv");
-// config()
-// const PORT = process.env.PORT || 3000;
+const express = require("express");
+const cors = require("cors");
+const bot = require("./server");
+const { webhookCallback } = require("grammy");
+const { config } = require("dotenv");
+config()
+const PORT = process.env.PORT;
 
-// const app = express()
-// let token = "5259090349:AAHQJzbGx-_mE1dmHec1IbP8F97t2ByIdXk"
-// app.use(cors())
-// app.use(express.json())
-// app.use(`/${token}`, webhookCallback(bot, "express"));
-// app.listen(PORT, '0.0.0.0', async () => {
-//     await bot.api.setWebhook(`https://tgbotenglish.herokuapp.com/${token}`);
-//     console.log(`server running ${PORT} in port`)
-// });
+const app = express()
+let token = "5259090349:AAHQJzbGx-_mE1dmHec1IbP8F97t2ByIdXk"
+app.use(cors())
+app.use(express.json())
+app.use(`/${token}`, webhookCallback(bot, "express"));
+app.listen(PORT || 3001, '0.0.0.0', async () => {
+    await bot.api.setWebhook(`https://tgbotenglish.herokuapp.com/${token}`);
+    console.log(`server running ${PORT} in port`)
+});
