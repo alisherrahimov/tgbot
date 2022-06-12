6804040c8fe65b2b9bb068bcee788970e5a19128
Alisher <49154018+alisherrahimov@users.noreply.github.com>
2022-06-12 14:31:31 +0500

    asdsad

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ const cors = require("cors");
 const bot = require("./bot");
 const { webhookCallback } = require("grammy");
 const { config } = require("dotenv");
-config()
-const PORT = process.env.PORT;
+config();
 
-const app = express()
-let token = "5259090349:AAHQJzbGx-_mE1dmHec1IbP8F97t2ByIdXk"
-app.use(cors())
+const app = express();
+let token = "5259090349:AAHQJzbGx-_mE1dmHec1IbP8F97t2ByIdXk";
+app.use(cors());
 app.use("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "Hello World"
-    })
-})
-app.use(express.json())
+  res.json({
+    success: true,
+    message: "Hello World",
+  });
+});
+app.use(express.json());
 app.use(`/${token}`, webhookCallback(bot, "express"));
-app.listen(PORT || 3001, '0.0.0.0', async () => {
-    await bot.api.setWebhook(`https://47.254.250.211:443/${token}`);
-    console.log(`server running ${PORT} in port`)
+app.listen(3001, "0.0.0.0", async () => {
+  await bot.api.setWebhook(`https://47.254.250.211:443/${token}`);
+  console.log(`server running ${PORT} in port`);
 });
